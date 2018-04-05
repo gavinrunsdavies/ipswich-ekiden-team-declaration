de9332ac66ac7dfa698443e112d79688a08baaef
Gavin Davies <gavin.davies@derivco.co.uk>
2018-04-05 15:20:10 +0100

    Admin UI enhancements

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 
+import { Team } from '../models/team';
 import { AuthService } from '../services/auth.service';
 import { TeamService } from '../services/team.service';
 import { MessageService } from '../services/message.service';
@@ -16,18 +17,23 @@ export class AdminComponent implements OnInit {
 
   seniorsCurrentPageNumber = 1;
   juniorsCurrentPageNumber = 1;
+  seniorTeamsCurrentPageNumber = 1;
+  juniorTeamsCurrentPageNumber = 1;
 
-  seniorData: any;
-  juniorData: any;
+  seniorData: any[];
+  juniorData: any[];
+  seniorTeams: Team[];
+  juniorTeams: Team[];
   juniorHeaders: string[];
   seniorHeaders: string[];
-  loadingIndicator = true;
+  loadingIndicator: any = {};
   formSubmittedIndicator = false;
-  email: any = {};
+  download: any = {};
 
   constructor(
     private router: Router,
     private authenticationService: AuthService,
+    private messageService: MessageService,
     private teamService: TeamService) { }
 
   ngOnInit() {
@@ -36,6 +42,8 @@ export class AdminComponent implements OnInit {
         user => {
           //  if (user.isAdmin) {
           this.getPreview();
+          this.getTeams('seniors', this.seniorTeams);
+          this.getTeams('juniors', this.juniorTeams);
           //      } else {
           //      this.router.navigate(['/']);
           //    }
@@ -45,8 +53,21 @@ export class AdminComponent implements OnInit {
         });
   }
 
+  getTeams(race: string, data: Team[]): void {
+    this.loadingIndicator[race] = true;
+    this.teamService.getTeams(race)
+      .subscribe(teams => {
+        if (race == 'seniors') {
+          this.seniorTeams = teams;
+        } else {
+          this.juniorTeams = teams;
+        }
+        this.loadingIndicator[race] = false;
+      });
+  }
+
   getPreview(): void {
-    this.loadingIndicator = true;
+    this.loadingIndicator['preview'] = true;
     this.teamService.getTeamDeclartionPreview()
       .subscribe(data => {
         this.seniorData = data.seniors;
@@ -59,15 +80,16 @@ export class AdminComponent implements OnInit {
           this.juniorHeaders = Object.getOwnPropertyNames(data.juniors[0]);
         }
 
-        this.loadingIndicator = false;
+        this.loadingIndicator['preview'] = false;
       }
       );
   }
 
   send(): void {
     this.formSubmittedIndicator = true;
-    this.teamService.sendTeamDeclarations(this.email)
+    this.teamService.sendTeamDeclarations(this.download.email)
       .subscribe(data => {
+        this.messageService.success(`Email sent to ${this.download.email} with team declaratiosn attached.`, true);
         this.formSubmittedIndicator = false;
       }
       );
